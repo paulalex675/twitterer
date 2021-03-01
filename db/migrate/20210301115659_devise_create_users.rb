b7b9253f5a81904e6cbbfb1328a80e6db7f40f71
Paul Robinson <paulalex675@hotmail.com>
2021-03-01 22:05:11 +0000

    Final parts added, didn't worry too much about profile pictures as lesson was mainly on devise

--- db/migrate/20210301115659_devise_create_users.rb
+++ db/migrate/20210301115659_devise_create_users.rb
@@ -15,11 +15,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+       t.datetime :current_sign_in_at
+       t.datetime :last_sign_in_at
+       t.string   :current_sign_in_ip
+       t.string   :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -42,3 +42,4 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
     # add_index :users, :unlock_token,         unique: true
   end
 end
+@tweeet = Tweeet.new
